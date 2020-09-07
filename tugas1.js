102954f80e9d6d27a0e324fbc5500e95c0cc7745
M. Ro'if <roifzubaidi5@gmail.com>
2020-09-08 00:58:42 +0700

    Tugas 1 JS
    Tugas ini kami kumpulkan untuk NIOMIC

---
+++ tugas1.js
@@ -0,0 +1,8 @@
+var nama_depan = "M. ";
+let nama_akhir = "Ro'if";
+const umur = 20;
+
+console.log(nama_depan);
+console.log(nama_akhir);
+console.log(umur);
+console.log(nama_depan + nama_akhir + " " + umur);
